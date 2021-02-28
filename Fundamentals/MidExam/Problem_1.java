54fc56432c38d6e085074ae8fdef89d83f6f3602
Denis Danailov <78208590+denisdanailov@users.noreply.github.com>
2021-02-28 09:47:50 +0100

    Add files via upload

---
+++ Fundamentals/MidExam/Problem_1.java
@@ -0,0 +1,35 @@
+package MidExam;
+
+import java.util.Scanner;
+
+public class Problem_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+        int students = Integer.parseInt(scanner.nextLine()) ;
+        double priceOfFlour = Double.parseDouble(scanner.nextLine());
+        double priceOfEgg = Double.parseDouble(scanner.nextLine());
+        double priceOfApron = Double.parseDouble(scanner.nextLine());
+
+        double countOfFlour = students;
+        if (students > 4) {
+
+            for (int i = 1; i <= students; i++) {
+                if (i % 5 == 0) {
+                    countOfFlour-= 1;
+                }
+            }
+        }
+        double result =  priceOfApron * (students + (Math.ceil(students * 0.20)))
+                + ((priceOfEgg * 10) * students)
+                + (priceOfFlour * countOfFlour);
+
+        if (budget >= result) {
+            System.out.println(String.format("Items purchased for %.2f$.",result));
+        } else {
+            double neededMoney = result - budget;
+            System.out.println(String.format("%.2f$ more needed.",neededMoney));
+        }
+    }
+}
