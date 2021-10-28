6b92ebb7c67fe51e194c6d3a763a96995a02afa8
Denis Danailov <denis.danailov@gmx.de>
2021-10-28 20:24:24 +0200

    Add Exercise

--- Js-Advanced/Exercise/Classes/AutoEngineeringCompany.js
+++ Js-Advanced/Exercise/Classes/AutoEngineeringCompany.js
@@ -8,16 +8,36 @@ function solve(input) {
 
         if (carBrands.has(brand)) {
             let carBrand = carBrands.get(brand);
-            if (carBrand.has(model)) {
+
+            if(carBrand.has(model)) {
                 let carModel = carBrand.get(model);
-               carModel += count;
-               carBrand.push(model, carModel);
+                carModel += count;
+                carBrand.set(model,carModel);
+
             } else {
-                carBrand.push(model,count);
+                    carBrand.set(model,count);
             }
 
         } else {
-            carBrands.push(brand, new Map());
+            carBrands.set(brand ,new Map());
+            carBrands.get(brand).set(model, count);
         }
-    })
+
+    });
+
+   for (const key of carBrands.keys()) {
+       console.log(key);
+       const brand = carBrands.get(key);
+       for (const [model, count] of brand) {
+           console.log(`###${model} -> ${count}`);
+       }
+   }
 }
+
+solve(['Audi | Q7 | 1000',
+    'Audi | Q7 | 200',
+    'Bmw | x3 | 2000',
+    'Ford | Mustang | 2009',
+    'Audi | Q7 | 2000'
+
+]);
