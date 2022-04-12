f4d674731a448426eb079dcf51d3386fea071fcc
Denis Danailov <denis.danailov@gmx.de>
2022-04-12 23:44:57 +0200

    Initial commit

---
+++ Data Structures and Algorithms (with Java)/ Linear-Data-Structures-Exercises-/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import implementations.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Queue<Integer> myQueue = new Queue<>();
+
+        myQueue.offer(1);
+        myQueue.offer(2);
+        myQueue.offer(3);
+        myQueue.offer(4);
+        myQueue.offer(5);
+
+    }
+}
---
+++ Data Structures and Algorithms (with Java)/ Linear-Data-Structures-Exercises-/src/main/java/implementations/ArrayDeque.java
@@ -0,0 +1,126 @@
+package implementations;
+
+import interfaces.Deque;
+
+import java.util.Iterator;
+
+public class ArrayDeque<E> implements Deque<E> {
+    private final int INITIAL_CAPACITY = 7;
+
+    private int size;
+    private int head;
+    private int tail;
+
+    private Object[] elements;
+
+    public ArrayDeque() {
+        this.elements = new Object[INITIAL_CAPACITY];
+        int middle = INITIAL_CAPACITY / 2;
+        head = tail = middle;
+    }
+
+    @Override
+    public void add(E Element) {
+
+    }
+
+    @Override
+    public void offer(E element) {
+
+    }
+
+    @Override
+    public void addFirst(E element) {
+
+    }
+
+    @Override
+    public void addLast(E element) {
+
+    }
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public void insert(int index, E element) {
+
+    }
+
+    @Override
+    public void set(int index, E element) {
+
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E get(Object object) {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E remove(Object object) {
+        return null;
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
+
+    @Override
+    public void trimToSize() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
